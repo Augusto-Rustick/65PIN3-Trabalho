19ea06e3a02c851bdd3b59f9682bbb5d88d6099d
Denis Zickuhr <coringatheboss@gmail.com>
2023-06-30 15:27:45 -0300

    changed bat var names

--- 65-PIN3/src/Bulk.bat
+++ 65-PIN3/src/Bulk.bat
@@ -1,6 +1,6 @@
 set smac=65-PIN3/src/solver/smac/runner_smac.py
-set irace=65-PIN3/src/solver/irace/starter.py
-set baseline=65-PIN3/src/solver/rs/baseline.py
+set irce=65-PIN3/src/solver/irace/starter.py
+set bsln=65-PIN3/src/solver/rs/baseline.py
 
 @REM python <solver> <net> <maxExperiments> <ExportToCsv> <Instances>
 
@@ -9,13 +9,15 @@ python %smac% nd 200 True
 python %smac% nd 300 True
 python %smac% nd 400 True
 python %smac% nd 500 True
-python %baseline% nd 100 True
-python %baseline% nd 200 True
-python %baseline% nd 300 True
-python %baseline% nd 400 True
-python %baseline% nd 500 True
-python %irace% nd 180 True
-python %irace% nd 200 True
-python %irace% nd 300 True
-python %irace% nd 400 True
-python %irace% nd 500 True
+
+python %bsln% nd 100 True
+python %bsln% nd 200 True
+python %bsln% nd 300 True
+python %bsln% nd 400 True
+python %bsln% nd 500 True
+
+python %irce% nd 180 True
+python %irce% nd 200 True
+python %irce% nd 300 True
+python %irce% nd 400 True
+python %irce% nd 500 True
